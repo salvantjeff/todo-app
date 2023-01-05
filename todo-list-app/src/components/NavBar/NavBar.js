d1464eaf9d09a7feb7d321285c21c4a987b24a39
Jeff-Salvant <salvant3jeff@gmail.com>
2023-01-05 14:51:17 -0500

    Toggle menu animation

--- todo-list-app/src/components/NavBar/NavBar.js
+++ todo-list-app/src/components/NavBar/NavBar.js
@@ -1,10 +1,25 @@
 import './NavBar.css';
+import { useEffect, useState } from 'react';
 
 function NavBar() {
+    const [menuStatus, setMenuStatus] = useState(false);
+    
+    function handleMenuClicked() {
+        setMenuStatus(!menuStatus);
+    };
+
+    useEffect(() => {
+        if (menuStatus) {
+            document.body.classList.add('active-menu');
+        } else {
+            document.body.classList.remove('active-menu');
+        }
+    }, [menuStatus]);
+
     return (
         <nav className="nav-bar">
             <div className="left-buttons">
-                <div>menu</div>
+                <div onClick={handleMenuClicked} >menu</div>
                 <div>home</div>
             </div>
             <div className="right-buttons">
