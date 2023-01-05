e7eeff50cd1d0c83470d5483c883ea31d5f01f91
Jeff-Salvant <salvant3jeff@gmail.com>
2023-01-05 15:53:21 -0500

    Create todo data

---
+++ todo-list-app/src/data/AllTodos.js
@@ -0,0 +1,12 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const AllTodos = [
+    {
+        name: 'Read a book',
+        description: 'Minimum of 25 pages',
+        date: 'Wed Jan 4',
+        id: uuidv4(),
+    },
+];
+
+export default AllTodos;
