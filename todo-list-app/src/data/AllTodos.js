3ffed837957f21997f70c56ac53fb6e52738d946
Jeff-Salvant <salvant3jeff@gmail.com>
2023-01-05 20:26:10 -0500

    Add priority property to todo data objects

--- todo-list-app/src/data/AllTodos.js
+++ todo-list-app/src/data/AllTodos.js
@@ -6,18 +6,21 @@ const AllTodos = [
         description: 'Minimum of 25 pages',
         date: 'Wed Jan 4',
         id: uuidv4(),
+        priority: 4,
     },
     {
         name: 'Meditate',
         description: 'Use waking up app',
         date: 'Wed Jan 4',
         id: uuidv4(),
+        priority: 4,
     },
     {
         name: 'Workout',
         description: 'Push day! lets go!',
         date: 'Wed Jan 4',
         id: uuidv4(),
+        priority: 4,
     },
 ];
 
