77d5f1bd8a16a7a2a480110b93a94eebbcc9393e
Jeff-Salvant <salvant3jeff@gmail.com>
2023-01-07 19:00:09 -0500

    Replace menu text with menu icon

--- todo-list-app/src/components/NavBar/NavBar.js
+++ todo-list-app/src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import './NavBar.css';
 import { useEffect, useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
 
 function NavBar() {
     const [menuStatus, setMenuStatus] = useState(false);
@@ -19,7 +20,7 @@ function NavBar() {
     return (
         <nav className="nav-bar">
             <div className="left-buttons">
-                <div onClick={handleMenuClicked} >menu</div>
+                <div onClick={handleMenuClicked}><AiOutlineMenu/></div>
                 <div>home</div>
             </div>
             <div className="right-buttons">
